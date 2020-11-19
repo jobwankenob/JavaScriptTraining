2b0ff0f27d0c02ec4294d6591c8476f31b56914f
Joel Lacroix <35319197+jobiwanken0bi@users.noreply.github.com>
2020-11-18 22:34:24 -0500

    devoir numero 1

---
+++ js/dynamic.js
@@ -0,0 +1,8 @@
+function valeurDeMaVariable() {
+    var nombreEntier = "1:10";
+    if (nombreEntier == "5") {
+        alert("la variable vaut 5");
+    } else {
+        alert("la variable ne vaut pas 5");
+    }
+}
