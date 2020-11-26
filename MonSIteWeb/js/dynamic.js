a0b9877fceb1d76287f3241ac88b06f59e6b0b3e
Joel Lacroix <35319197+jobwankenob@users.noreply.github.com>
2020-11-26 08:48:14 -0500

    Delete dynamic.js

--- MonSIteWeb/js/dynamic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function valeurDeMaVariable() {
-    var x = 4;
-    if (x == 5) {
-        alert("la variable vaut 5");
-    } else {
-        alert("la variable ne vaut pas 5");
-    }
-}
